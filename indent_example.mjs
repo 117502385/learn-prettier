8d1cdef01a2839eefcc8da9f8cd80491aafe466f
117502385 <117502385@qq.com>
2025-09-25 10:33:04 +0800

    indent_example 智能换行

--- indent_example.mjs
+++ indent_example.mjs
@@ -1,11 +1,12 @@
 import prettier from 'prettier';
 
-const {indent, concat, line} = prettier.doc.builders;
+const {group, indent, concat, line} = prettier.doc.builders;
 
 const doc = concat(['[', indent(concat([line, 'hello'])), line, ']']);
 
-const ps = prettier.doc.printer.printDocToString(doc, {
-    tabWidth: 4, // 缩进宽度
+const ps = prettier.doc.printer.printDocToString(group(doc), {
+    tabWidth: 4,
+    printWidth: 80,
 });
 
 console.log(ps.formatted);
