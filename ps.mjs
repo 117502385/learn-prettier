b8b54b83e872c0d673ad56479bda7df6cd12627c
117502385 <117502385@qq.com>
2025-09-27 00:05:53 +0800

    join

--- ps.mjs
+++ ps.mjs
@@ -1,12 +1,12 @@
 import prettier from 'prettier';
 
-export function ps(doc, width = 80, mode = {log: true, ret: true}) {
+export function ps(doc, printWidth = 80, mode = {log: true, ret: true}) {
     const {formatted} = prettier.doc.printer.printDocToString(doc, {
-        printWidth: width,
+        printWidth,
         tabWidth: 4,
     });
-    if (mode.log)
-        console.log(formatted);
+
+    if (mode.log) console.log(formatted);
 
     return mode.ret ? formatted : undefined;
 }
